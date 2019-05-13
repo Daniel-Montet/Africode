bfc14152b662a11189053cf445d729331e303677
Daniel Montet <dmontetproff@gmail.com>
2019-05-13 12:06:08 +0300

    Add model tests

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.utils import timezone
 
+from .models import Course, Step
 # Create your tests here.
+
+class CourseModelTests(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(
+            title= "Python regular Expressions",
+            description = "Learn to write regular expressions"
+        )
+        now = timezone.now()
+        self.course=course
+        self.assertLess(course.created_at, now)
+
+    # def test_step_creation(self):
+    #     step = Step.objects.create(
+    #         title = "Sample test",
+    #         description = "This is a sample test",
+    #         content = "This is some sample content",
+    #         course= self.course
+    #     )
+    #     now = timezone.now()
+    #     self.assertLess(course.created_at, now)
